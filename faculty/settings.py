32c364b02aadd982aff35890a2999d7d729d4cc4
Vishnu Charan Venkatesh <76093350+VishnucharanV@users.noreply.github.com>
2021-06-15 14:35:59 +0530

    Update settings.py

--- faculty/settings.py
+++ faculty/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b5s95&x*%13e-nc7nk*ln*q!x%ki*q)g@bdxpm+cs@n&*-sazi'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'faculty.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'backend',
-        'USER': 'postgres',
-        'PASSWORD': 'sudanve5831',
-        'HOST' : 'localhost'
+        'ENGINE': 'django.db.#.postgresql',
+        'NAME': '#',
+        'USER': '#',
+        'PASSWORD': '#',
+        'HOST' : '#'
     }
 }
 
@@ -132,10 +132,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 #EMAIL SETTINGS
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = '18cs160@psgitech.ac.in'
-EMAIL_HOST_PASSWORD = 'sudanve5831'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = '#'
+EMAIL_PORT = #
+EMAIL_HOST_USER = '#'
+EMAIL_HOST_PASSWORD = '#'
+EMAIL_USE_TLS = #
+EMAIL_USE_SSL = #
+EMAIL_BACKEND = '#'
